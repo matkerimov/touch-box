64550226ceaa286e6513494a8e85f1e89411f968
zhaiyl07 <matkerimov.zhaiyl.ib9.1.19@gmail.com>
2021-08-04 18:23:53 +0600

    fdsdf

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
+const wrapper = document.querySelector(".boxes")
 const changeColor = document.querySelectorAll( ".background")
 const colors = ["color", "blue", "green", "red"]
 
-    changeColor.forEach((el )=> {
-        el.addEventListener("click", () =>{
-            const random = Math.floor(Math.random() * 4)
-            changeColor.forEach((el) =>{
-                el.removeAttribute("class")
-            })
-            el.classList.add(colors[random])
-        })
-    })
+wrapper.addEventListener("click", (event) => {
+    
+})
+    // changeColor.forEach((el )=> {
+    //     el.addEventListener("click", () =>{
+    //         const random = Math.floor(Math.random() * 4)
+    //         changeColor.forEach((el) =>{
+    //             el.removeAttribute("class")
+    //         })
+    //         el.classList.add(colors[random])
+    //     })
+    // })
 
 
 
